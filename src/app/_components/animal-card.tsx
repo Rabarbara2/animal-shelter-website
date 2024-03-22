b934868dd38b4ba37bb3434b23aa4db3f14c3952
Toster <ebebebeawdawdaw@gmail.com>
2024-03-22 19:02:12 +0100

    start adoption page

--- src/app/_components/animal-card.tsx
+++ src/app/_components/animal-card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import MaleSymbol from "../assets/male-symbol";
 import FemaleSymbol from "../assets/female-symbol";
 import { RouterOutputs } from "~/trpc/shared";
+import Link from "next/link";
 
 type Cat = RouterOutputs["cat"]["getAll"]["0"];
 type HealthIssues = RouterOutputs["healthIssue"]["getAll"];
@@ -16,7 +17,18 @@ export default function AnimalCard({
   healthIssues,
 }: AnimalCardProps) {
   return (
-    <div className="flex  w-[21rem] flex-col items-center rounded-xl bg-red-50  shadow transition-colors duration-100 ease-in-out hover:bg-white">
+    <Link
+      href={"/"}
+      className="flex w-64 flex-col items-center rounded-3xl bg-red-50 
+       shadow-md shadow-slate-800/30 transition-all duration-200 ease-in-out hover:-translate-y-2 hover:bg-white "
+    >
+      <Image
+        src={image}
+        width={288}
+        height={384}
+        alt="animal"
+        className="🐷 h-72 w-64 rounded-t-2xl border-b border-slate-800 object-cover"
+      />
       <div className="flex items-center justify-center">
         <div className=" p-4 text-2xl font-semibold">{name}</div>
         <div>
@@ -26,40 +38,29 @@ export default function AnimalCard({
             <FemaleSymbol className=" h-10 w-10 bg-clip-content fill-pink-400" />
           )}
         </div>
-      </div>
-      <Image
-        src={image}
-        width={288}
-        height={384}
-        alt="animal"
-        className="🐷 h-96 w-72 rounded-xl border-4 object-cover"
-      />
-      <div className="flex min-h-[40px] justify-center">
-        <div className="flex items-center last:rounded-r">
-          {catHealthRecords.map((record) => {
-            const healthIssue = healthIssues.find(
-              ({ id }) => id === Number(record.healthIssueId),
-            );
+        <div className="flex min-h-[40px] justify-center">
+          <div className="flex items-center last:rounded-r">
+            {catHealthRecords.map((record) => {
+              const healthIssue = healthIssues.find(
+                ({ id }) => id === Number(record.healthIssueId),
+              );
 
-            if (!healthIssue) {
-              return null;
-            }
+              if (!healthIssue || healthIssue.type === "vaccine") {
+                return null;
+              }
 
-            return (
-              <div
-                key={`${record.catId} ${record.healthIssueId}`}
-                className={`border ${healthIssue.type === "disease" ? "border-red-800 bg-red-600" : "border-teal-800 bg-teal-600"} p-1 text-[8px] text-white first:rounded-l last:rounded-r`}
-              >
-                {
-                  healthIssues.find(
-                    ({ id }) => id === Number(record.healthIssueId),
-                  )?.name
-                }
-              </div>
-            );
-          })}
+              return (
+                <div
+                  key={`${record.catId} ${record.healthIssueId}`}
+                  className={`border border-red-800 bg-red-600 p-1 text-[8px] text-white first:rounded-l last:rounded-r`}
+                >
+                  {healthIssue.name}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
--- src/app/adoption/page.tsx
+++ src/app/adoption/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import Navbar from "../_components/navbar";
 
 export default async function Adoption() {
   return (
-    <>
-      <div> adoption</div>
-      <Link href={"/adoption/dogs"}>woof</Link>
-      <Link href={"/adoption/cats"}>meow</Link>
-      <Link href={"/adoption/others"}>grrru?</Link>
-    </>
+    <main className="flex min-h-screen justify-center bg-zinc-900">
+      <Navbar />
+      <div className="min-h-screen w-5/6 bg-pink-900">
+        <Link href={"/adoption/dogs"}>woof</Link>
+        <Link href={"/adoption/cats"}>meow</Link>
+        <Link href={"/adoption/others"}>grrru?</Link>
+      </div>
+    </main>
   );
 }
