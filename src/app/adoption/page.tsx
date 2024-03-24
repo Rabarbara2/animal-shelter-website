73cacfc3bb62e6e4c5ca92f0d19d15863670026e
Toster <ebebebeawdawdaw@gmail.com>
2024-03-24 19:32:50 +0100

    finnish adoption page

--- src/app/adoption/page.tsx
+++ src/app/adoption/page.tsx
@@ -3,9 +3,12 @@ import Navbar from "../_components/navbar";
 import animals from "../assets/animals.jpg";
 import cat from "../assets/cat_in_house.jpg";
 import dog from "../assets/dow_in_house.jpg";
+
+import doga from "../assets/Adopt.jpg";
+import cata from "../assets/Vol.jpg";
+import birda from "../assets/birb.jpg";
 import Image from "next/image";
 import MainPageAdopt from "../_components/main-page-adopt";
-
 export default async function Adoption() {
   return (
     <main className="flex min-h-screen justify-center bg-zinc-900">
@@ -68,12 +71,58 @@ export default async function Adoption() {
             </div>
           </div>
         </div>
-        {/* TODO */}
+
         <div className=" w-full p-8 text-left text-3xl font-medium text-white">
-          todo
-          <br /> See the animals currently available for adoption!
+          See the animals currently available for adoption!
         </div>
-        <MainPageAdopt />
+
+        <div className="flex  flex-row justify-center gap-12 p-6 pb-14">
+          <Link
+            href={"/adoption/dogs"}
+            className="flex h-fit w-[30%] flex-col items-center rounded-xl bg-red-50 p-6 shadow hover:bg-white  hover:text-pink-700"
+          >
+            <div className="p-2 text-4xl font-semibold ">Dogs</div>
+            <Image
+              src={doga}
+              alt="animal"
+              className="h-auto w-auto scale-95 rounded-xl"
+            />
+            <div className="mt-2 text-center text-zinc-800">
+              Meet our lovely dogs and choose one to become your new best
+              friend!
+            </div>
+          </Link>
+          <Link
+            href={"/adoption/cats"}
+            className="flex h-fit w-[30%] flex-col items-center rounded-xl bg-red-50 p-6 shadow hover:bg-white  hover:text-pink-700"
+          >
+            <div className="p-2 text-4xl font-semibold ">Cats</div>
+            <Image
+              src={cata}
+              alt="animal"
+              className="h-auto w-auto scale-95 rounded-xl"
+            />
+            <div className="mt-2 text-center text-zinc-800">
+              Meet our lovely cats and choose one (or more!) to become your new
+              best friend!
+            </div>
+          </Link>
+          <Link
+            href={"/adoption/others"}
+            className="flex h-fit w-[30%] flex-col items-center rounded-xl bg-red-50 p-6 shadow hover:bg-white  hover:text-pink-700"
+          >
+            <div className="p-2 text-4xl font-semibold ">Other pets</div>
+            <Image
+              src={birda}
+              alt="animal"
+              className="h-auto w-auto scale-95 rounded-xl"
+            />
+            <div className="mt-2 text-center text-zinc-800">
+              Meet our lovely pets and choose one (or more!) to become your new
+              best friend!
+            </div>
+          </Link>
+        </div>
       </div>
     </main>
   );
