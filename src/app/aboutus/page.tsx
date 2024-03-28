18f63cd52fe0751769643c7f19c137d44bbb1b0f
Toster <ebebebeawdawdaw@gmail.com>
2024-03-28 21:06:06 +0100

    add hero to about us page

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,9 +1,22 @@
+import obrazek from "../assets/animals.jpg";
 import Navbar from "../_components/navbar";
+import Image from "next/image";
 
 export default async function Aboutus() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center bg-zinc-800">
       <Navbar />
+      <div className="relative flex w-5/6 flex-col items-center justify-center overflow-hidden">
+        <Image src={obrazek} alt="hero" className="  brightness-[70%]" />
+        <div className="absolute left-8 ">
+          <div className="p-2 text-5xl font-semibold text-white drop-shadow-xl">
+            We help animals
+          </div>
+          <div className="ml-10 p-4 text-lg text-white drop-shadow-lg">
+            Animals are super cool in our opinion
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
