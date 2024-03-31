bfb4495b4a422ce672eaa0a672ffe0ae9c91ab1e
Toster <ebebebeawdawdaw@gmail.com>
2024-03-31 20:46:17 +0200

    add text to about us page

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -22,7 +22,27 @@ export default async function Aboutus() {
         <Triangle fill="white" className="brightness-[70%]" />
       </div>
       <div className="h-96 w-5/6 bg-pink-900 p-12 text-lg text-white">
-        We are helping cats and dogs and birds.
+        At [Animal Shelter Name], we believe that every life is precious and
+        deserving of love and care. Our dedicated team of staff and volunteers
+        work around the clock to ensure that each animal that comes through our
+        doors receives the attention, medical care, and support they need to
+        thrive. Our shelter is more than just a place for animals to stay
+        temporarily; it's a sanctuary where they are treated with kindness and
+        respect. We provide a nurturing environment where they can heal from
+        past traumas, receive training and socialization, and ultimately find
+        loving forever homes. In addition to our commitment to the welfare of
+        individual animals, we are also passionate about addressing broader
+        issues such as pet overpopulation and animal welfare education. Through
+        our spay/neuter programs, community outreach efforts, and educational
+        initiatives, we strive to make a positive impact on the lives of animals
+        both within our shelter and beyond. None of our work would be possible
+        without the generous support of our community. Whether through
+        donations, volunteering, or adopting, every contribution helps us
+        continue our vital work of saving lives and making a difference. Join us
+        in our mission to create a world where every animal is valued and
+        cherished. Together, we can make a difference, one paw at a time. Thank
+        you for supporting [Animal Shelter Name] and being a part of our
+        journey.
       </div>
     </div>
   );
