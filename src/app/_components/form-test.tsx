5dabb6281288b5682411170dde733045ea552c48
Toster <ebebebeawdawdaw@gmail.com>
2024-04-05 21:31:49 +0200

    change form

--- src/app/_components/form-test.tsx
+++ src/app/_components/form-test.tsx
@@ -15,18 +15,19 @@ export default function FormTest() {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  console.log(watch("example"), errors); // watch input value by passing the name of it
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+      <input defaultValue="test" {...register("example", {})} minLength={5} />
 
       {/* include validation with required or other standard HTML validation rules */}
       <input {...register("exampleRequired", { required: true })} />
       {/* errors will return when field validation fails  */}
       {errors.exampleRequired && <span>This field is required</span>}
+      {errors.example && <span>{errors.example.message}</span>}
 
       <input type="submit" />
     </form>
