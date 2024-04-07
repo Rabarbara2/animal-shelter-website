4b2ecde5a0b58e2c17411c87cc06244403e75cb9
Toster <ebebebeawdawdaw@gmail.com>
2024-04-07 19:24:21 +0200

    style form

--- src/app/_components/form-test.tsx
+++ src/app/_components/form-test.tsx
@@ -23,27 +23,47 @@ export default function FormTest() {
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex w-5/6 flex-col">
       {/* register your input into the hook by invoking the "register" function */}
       <div>name:</div>
-      <input defaultValue="" {...register("name", {})} />
+      <input
+        defaultValue=""
+        {...register("name", {})}
+        className="  w-1/3 p-1 text-lg"
+      />
       <div>colour:</div>
-      <input defaultValue="" {...register("colour", {})} />
+      <input
+        defaultValue=""
+        {...register("colour", {})}
+        className=" w-1/3 p-1 text-lg "
+      />
       <div>image link: </div>
-      <input defaultValue="" {...register("imageLink", {})} />
+      <input
+        defaultValue=""
+        {...register("imageLink", {})}
+        className=" w-1/3 p-1 text-lg "
+      />
       <div>gender: </div>
 
-      <select {...register("gender", {})}>
+      <select {...register("gender", {})} className=" w-1/3 p-1 text-lg ">
         <option value="male">Male</option>
         <option value="female">Female</option>
       </select>
 
       <div>date of birth: </div>
-      <input type="date" {...register("dateOfBirth", {})} />
+      <input
+        type="date"
+        {...register("dateOfBirth", {})}
+        className=" w-1/3 p-1 text-lg "
+      />
       <div>estimated? </div>
-      <input type="checkbox" {...register("estimated", {})} />
+      <input
+        type="checkbox"
+        {...register("estimated", {})}
+        className=" w-1/3 p-1 text-lg "
+      />
       <div>fur length: </div>
-      <select {...register("furLength", {})}>
+      <select {...register("furLength", {})} className=" w-1/3 p-1 text-lg ">
         <option value="bald">Bald</option>
         <option value="short">Short</option>
         <option value="medium">Medium</option>
@@ -52,9 +72,17 @@ export default function FormTest() {
       </select>
 
       <div>race: </div>
-      <input defaultValue="" {...register("race", {})} />
+      <input
+        defaultValue=""
+        {...register("race", {})}
+        className=" w-1/3 p-1 text-lg "
+      />
 
-      <input type="submit" />
+      <input
+        type="submit"
+        className="w-fit cursor-pointer rounded bg-lime-500 p-3"
+        value="submit"
+      />
     </form>
   );
 }
