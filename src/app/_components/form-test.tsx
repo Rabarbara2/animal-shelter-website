cc900487f9356181b1c652c6649da5497cf1cc97
Toster <ebebebeawdawdaw@gmail.com>
2024-04-08 21:42:32 +0200

    image preview test

--- src/app/_components/form-test.tsx
+++ src/app/_components/form-test.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useForm, SubmitHandler } from "react-hook-form";
+import Image from "next/image";
 
 type Inputs = {
   name: string;
@@ -22,67 +23,92 @@ export default function FormTest() {
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)} className="flex w-5/6 flex-col">
-      {/* register your input into the hook by invoking the "register" function */}
-      <div>name:</div>
-      <input
-        defaultValue=""
-        {...register("name", {})}
-        className="  w-1/3 p-1 text-lg"
-      />
-      <div>colour:</div>
-      <input
-        defaultValue=""
-        {...register("colour", {})}
-        className=" w-1/3 p-1 text-lg "
-      />
-      <div>image link: </div>
-      <input
-        defaultValue=""
-        {...register("imageLink", {})}
-        className=" w-1/3 p-1 text-lg "
-      />
-      <div>gender: </div>
+    <div className="flex w-5/6 justify-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex w-full flex-col">
+        <div>
+          <div>name:</div>
 
-      <select {...register("gender", {})} className=" w-1/3 p-1 text-lg ">
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-      </select>
+          <input
+            defaultValue=""
+            {...register("name", {})}
+            className="  w-1/3 p-1 text-lg"
+          />
+        </div>
+        <div>
+          <div>colour:</div>
 
-      <div>date of birth: </div>
-      <input
-        type="date"
-        {...register("dateOfBirth", {})}
-        className=" w-1/3 p-1 text-lg "
-      />
-      <div>estimated? </div>
-      <input
-        type="checkbox"
-        {...register("estimated", {})}
-        className=" w-1/3 p-1 text-lg "
-      />
-      <div>fur length: </div>
-      <select {...register("furLength", {})} className=" w-1/3 p-1 text-lg ">
-        <option value="bald">Bald</option>
-        <option value="short">Short</option>
-        <option value="medium">Medium</option>
-        <option value="long">Long</option>
-        <option value="unknown">Unknown</option>
-      </select>
+          <input
+            defaultValue=""
+            {...register("colour", {})}
+            className=" w-1/3 p-1 text-lg "
+          />
+        </div>
+        <div>
+          <div>image link: </div>
 
-      <div>race: </div>
-      <input
-        defaultValue=""
-        {...register("race", {})}
-        className=" w-1/3 p-1 text-lg "
-      />
+          <input
+            defaultValue=""
+            {...register("imageLink", {})}
+            className=" w-1/3 p-1 text-lg "
+          />
+        </div>
+        <div>
+          <div>gender: </div>
 
-      <input
-        type="submit"
-        className="w-fit cursor-pointer rounded bg-lime-500 p-3"
-        value="submit"
-      />
-    </form>
+          <select {...register("gender", {})} className=" w-1/3 p-1 text-lg ">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div>
+          <div>date of birth: </div>
+
+          <input
+            type="date"
+            {...register("dateOfBirth", {})}
+            className=" w-1/3 p-1 text-lg "
+          />
+        </div>
+        <label>
+          estimated?
+          <input
+            type="checkbox"
+            {...register("estimated", {})}
+            className="m-2 p-1 text-lg "
+          />
+        </label>
+        <div>
+          <div>fur length: </div>
+          <select
+            {...register("furLength", {})}
+            className=" w-1/3 p-1 text-lg "
+          >
+            <option value="bald">Bald</option>
+            <option value="short">Short</option>
+            <option value="medium">Medium</option>
+            <option value="long">Long</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </div>
+        <div>
+          <div>race: </div>
+
+          <input
+            defaultValue=""
+            {...register("race", {})}
+            className=" w-1/3 p-1 text-lg "
+          />
+        </div>
+
+        <input
+          type="submit"
+          className="w-fit cursor-pointer rounded bg-lime-500 p-3"
+          value="submit"
+        />
+      </form>
+      <div className="relative h-96 w-96">
+        <Image src={watch("imageLink")} alt="woof" fill />
+      </div>
+    </div>
   );
 }
