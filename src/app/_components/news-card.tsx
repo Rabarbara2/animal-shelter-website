f9464ced7faa87a8543ad6041d8698bc09c0500e
Toster <ebebebeawdawdaw@gmail.com>
2024-05-17 20:28:43 +0200

    add markdown reader to articles

--- src/app/_components/news-card.tsx
+++ src/app/_components/news-card.tsx
@@ -3,12 +3,16 @@ import Image from "next/image";
 import adopt from "../assets/Foster.jpg";
 import Link from "next/link";
 import React, { useState } from "react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-export default function NewsCard(props: {
+type NewsCardProps = {
   category: string;
   text: string;
   createdAt: Date;
-}) {
+};
+
+export default function NewsCard({ category, createdAt, text }: NewsCardProps) {
   const [isActive, setIsActive] = useState(false);
   return (
     <div className="relative flex h-fit w-5/6 flex-col gap-4 rounded-xl bg-red-50 p-6 shadow lg:flex-row lg:items-stretch">
@@ -21,10 +25,10 @@ export default function NewsCard(props: {
         <div className="flex flex-col gap-2">
           <div className="flex items-center gap-3">
             <div className="w-fit rounded bg-pink-950 p-2 text-base font-semibold text-white">
-              {props.category.charAt(0).toUpperCase() + props.category.slice(1)}
+              {category.charAt(0).toUpperCase() + category.slice(1)}
             </div>
             <div className="w-fit text-sm text-stone-600">
-              {props.createdAt.toLocaleString([], {
+              {createdAt.toLocaleString([], {
                 year: "numeric",
                 month: "numeric",
                 day: "numeric",
@@ -32,13 +36,15 @@ export default function NewsCard(props: {
             </div>
             <div />
           </div>
-          <div
+          <Markdown remarkPlugins={[remarkGfm]}>{text}</Markdown>
+
+          {/* <div
             dangerouslySetInnerHTML={{
-              __html: props.text,
+              __html: text,
             }}
             className={`w-auto overflow-hidden whitespace-pre-wrap text-justify text-lg font-normal text-zinc-800 transition-all duration-700 ease-in-out
               ${isActive ? " max-h-[1500px]" : "max-h-[140px]"}`}
-          ></div>
+          ></div> */}
         </div>
 
         <button
